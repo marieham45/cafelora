4ab1dba7d118ad4f9a2d4fe81411ab1bdc2de2aa
marieham45 <marie.hamsikova@centrum.cz>
2022-11-29 20:26:16 +0100

    Zobrazování Layer v Menu pomocí komponenty

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 
+import { Layer } from "./components/Layer/index.js";
+
 export const Menu = () => {
   const element = document.createElement("section");
 
@@ -18,62 +20,26 @@ export const Menu = () => {
         </div>
         <div class="drink__info">
           <h3>Espresso</h3>
-          <div class="layer">
-            <div class="layer__color" style="background-color: #613916"></div>
-            <div class="layer__label">espresso</div>
-          </div>
-        </div>
+                  </div>
       </div>
       <div class="drink__controls">
         <button class="order-btn">Objednat</button>
       </div>
     </div>
 
-    <div class="drink">
-      <div class="drink__product">
-        <div class="drink__cup">
-          <img src="https://apps.kodim.cz/daweb/cafelora/assets/cups/doppio.png" />
-        </div>
-        <div class="drink__info">
-          <h3>Doppio</h3>
-          <div class="layer">
-            <div class="layer__color" style="background-color: #613916"></div>
-            <div class="layer__label">espresso</div>
-          </div>
-        </div>
-      </div>
-      <div class="drink__controls">
-        <button class="order-btn">Objednat</button>
-      </div>
-    </div>
 
-    <div class="drink">
-      <div class="drink__product">
-        <div class="drink__cup">
-          <img src="https://apps.kodim.cz/daweb/cafelora/assets/cups/lungo.png" />
-        </div>
-        <div class="drink__info">
-          <h3>Lungo</h3>
-          <div class="layer">
-            <div class="layer__color" style="background-color: #b0dee1"></div>
-            <div class="layer__label">voda</div>
-          </div>
-          <div class="layer">
-            <div class="layer__color" style="background-color: #613916"></div>
-            <div class="layer__label">espresso</div>
-          </div>
-        </div>
-      </div>
-      <div class="drink__controls">
-        <button class="order-btn">Objednat</button>
-      </div>
-    </div>
+
+
   </div>
 
   <div class="order-detail">
     <a href="/objednavka">Detail objednávky</a>
   </div>
 </div>`;
+
+  element
+    .querySelector(".drink__info")
+    .append(Layer({ color: "#613916", label: "espresso" }));
 
   return element;
 };
