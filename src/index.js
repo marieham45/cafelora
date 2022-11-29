88418204defdc8a234e44535a65b9771d534a434
marieham45 <marie.hamsikova@centrum.cz>
2022-11-29 19:44:11 +0100

    Přidání komponent a rozdělení stylů

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './style.css';
+import "./style.css";
 
-console.log('funguju!');
+import { Banner } from "./components/Banner/index.js";
+import { Contact } from "./components/Contact/index.js";
+import { Footer } from "./components/Footer/index.js";
+import { Gallery } from "./components/Gallery/index.js";
+import { Header } from "./components/Header/index.js";
+import { Menu } from "./components/Menu/index.js";
+
+console.log("funguju!");
+
+const pageElement = document.createElement("div");
+pageElement.classList.add("page");
+
+const main = document.createElement("main");
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector("#app").append(pageElement);
